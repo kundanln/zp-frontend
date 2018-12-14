cff1ca7fc9575b2b5dcebf6a7cc8ffe149492be5
kundanln <kundannarkhede93@gmail.com>
2018-12-14 06:33:12 -0800

    service

--- src/app/_services/loan-payment.service.ts
+++ src/app/_services/loan-payment.service.ts
@@ -15,33 +15,33 @@ export class LoanPaymentService {
     create(record: any) {
 
        // console.log("record ",JSON.stringify(record));
-        return this._http.post(this._baseService.BaseUrl()+'/add',record);
+        return this._http.post(this._baseService.BaseUrl()+'/loan-details/add',record);
 
     }
 
     update(record: any) {
         //console.log("at enquiry service ",enquiry);
-        return this._http.put(this._baseService.BaseUrl() + '/update/id/' + record.id, record);
+        return this._http.put(this._baseService.BaseUrl() + '/loan-details/update/id/' + record.id, record);
     }
 
     getAll() {
 
         // Make the GET HTTP request:
-        return this._http.get(this._baseService.BaseUrl() + "/getAll");
+        return this._http.get(this._baseService.BaseUrl() + "/loan-details/getAll");
         // return this._http.get<User[]>('/api/users');
     }
 
     getRecoredById(id : Number)
     {
         //reg id
-        return this._http.get<any>(this._baseService.BaseUrl()+"/getById/id/"+ id);
+        return this._http.get<any>(this._baseService.BaseUrl()+"/loan-details/getById/id/"+ id);
     }
 
     getRecordaForCalculateInterest(zpId: number){
-        return this._http.get<any>(this._baseService.BaseUrl()+"/getForCalInterest/id/"+ zpId);
+        return this._http.get<any>(this._baseService.BaseUrl()+"/loan-details/getForCalInterest/id/"+ zpId);
     }
 
     getLoanDetailsRecordsByZpId(): any {
-        return this._http.get(this._baseService.BaseUrl() + "/getLoanDetailsRecordsByZpId");
+        return this._http.get(this._baseService.BaseUrl() + "/loan-details/getLoanDetailsRecordsByZpId");
       }
 }
--- src/app/_services/village.service.ts
+++ src/app/_services/village.service.ts
@@ -14,29 +14,29 @@ export class VillageService {
     create(record: any) {
 
        // console.log("record ",JSON.stringify(record));
-        return this._http.post(this._baseService.BaseUrl()+'/add',record);
+        return this._http.post(this._baseService.BaseUrl()+'/village/add',record);
 
     }
 
     update(record: any) {
         //console.log("at enquiry service ",enquiry);
-        return this._http.put(this._baseService.BaseUrl() + '/update/id/' + record.id, record);
+        return this._http.put(this._baseService.BaseUrl() + '/village/update/id/' + record.id, record);
     }
     
     getAll() {
 
         // Make the GET HTTP request:
-        return this._http.get(this._baseService.BaseUrl() + "/getAll");
+        return this._http.get(this._baseService.BaseUrl() + "/village/getAll");
         // return this._http.get<User[]>('/api/users');
     }
 
     getRecoredById(id : Number)
     {
         //reg id
-        return this._http.get<any>(this._baseService.BaseUrl()+"/getById/id/"+ id);
+        return this._http.get<any>(this._baseService.BaseUrl()+"/village/getById/id/"+ id);
     }
     delete(id: number) {
-        return this._http.delete(this._baseService.BaseUrl()+'/delete/id/' + id);
+        return this._http.delete(this._baseService.BaseUrl()+'/village/delete/id/' + id);
     }
 
 }
